01dfb6653690398fd9dcda807ff05ebb81e49c28
GoHakNeung <98439777+GoHakNeung@users.noreply.github.com>
2024-08-28 09:55:16 +0900

    Update globals_variable.py

--- globals_variable.py
+++ globals_variable.py
@@ -2,6 +2,8 @@
 question_num = None
 total_attempts = None
 final_result = False
+result_df = pd.DataFrame(columns = ['id', 'question_file', 'final_result', 'total_attempts']) 
+
 
 question_name = None
 question_name_data = None
