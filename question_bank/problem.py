92b3b9df245de2c12663433e197331f9737c41ac
GoHakNeung <98439777+GoHakNeung@users.noreply.github.com>
2024-08-28 10:20:07 +0900

    Update problem.py

--- question_bank/problem.py
+++ question_bank/problem.py
@@ -9,7 +9,8 @@ def Question(question_number, _type = 'code'):
     globals_variable.question_num = question_number
 
     if question_number in list(globals_variable.result_df['id']) : 
-        globals_variable.total_attempts = globals_variable.result_df[globals_variable.result_df['id'] == question_number]['total_attempts'].loc[0]
+        print(globals_variable.result_df[globals_variable.result_df['id'] == question_number]['total_attempts'])
+        # globals_variable.total_attempts = globals_variable.result_df[globals_variable.result_df['id'] == question_number]['total_attempts'].loc[0]
         globals_variable.final_result = False
     else : 
         globals_variable.total_attempts = 0
